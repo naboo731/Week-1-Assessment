9c1339dfe03c6e292920523970fc3900a7923530
Nichole Boothman <nbooth731@gmail.com>
2021-11-12 12:44:01 -0500

    password validator works

--- password.js
+++ password.js
@@ -5,3 +5,18 @@
     // At least 10 characters longIf the user’s password meets the constraint, show a success message to the user
     // If the user’s password fails the constraint, show a failure message to the user
 
+console.log("Welcome to the Password Validator Tool!")
+const readline = require('readline')
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+reader.question("What is your password?", function(input){
+    
+    if(input.length < 10){
+        console.log("Please provide password that is at least 10 characters long.")
+    } else console.log("Congratulations! Your password is valid.")
+
+})
