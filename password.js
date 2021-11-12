1f48cba8c61455205298c090c73654827d14b1eb
Nichole Boothman <nbooth731@gmail.com>
2021-11-12 12:21:05 -0500

    inital commit

---
+++ password.js
@@ -0,0 +1,7 @@
+// Write a program that does the following:
+    // Welcome the user to the password validator tool
+    // Prompt the user for a password to validate
+    // Check if the user’s password meets the following constraint:
+    // At least 10 characters longIf the user’s password meets the constraint, show a success message to the user
+    // If the user’s password fails the constraint, show a failure message to the user
+
